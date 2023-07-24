2b6e811254a7c3114857b3c8b9e0a615eecf8692
Monly44 <manusic44@gmail.com>
2023-07-24 17:08:21 +0200

    zadaca

--- src/Components/UserForm.js
+++ src/Components/UserForm.js
@@ -6,29 +6,23 @@ function UserForm() {
   const { setUsers } = useContext(UserContext);
   const [ime, setIme] = useState("");
   const [prezime, setPrezime] = useState("");
-  const [error, setError] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (ime.trim() === "" || prezime.trim() === "") {
-      setError("Molimo unesite ime i prezime korisnika.");
-      return;
-    }
-
+    if (ime.trim() === "" || prezime.trim() === "") return;
     setUsers((prevUsers) => [
       ...prevUsers,
       { id: prevUsers.length + 1, ime: ime, prezime: prezime },
     ]);
     setIme("");
     setPrezime("");
-    setError("");
   };
 
   return (
     <form onSubmit={handleSubmit} className="user-form">
       <div className="form-group">
-        <label htmlFor="ime">Ime:</label>
         <input
+          placeholder="Ime"
           className="input-field"
           type="text"
           id="ime"
@@ -37,8 +31,8 @@ function UserForm() {
         />
       </div>
       <div className="form-group">
-        <label htmlFor="prezime">Prezime:</label>
         <input
+          placeholder="Prezime"
           className="input-field"
           type="text"
           id="prezime"
@@ -47,7 +41,6 @@ function UserForm() {
         />
       </div>
       <button type="submit">Dodaj korisnika</button>
-      {error && <p className="error-message">{error}</p>}
     </form>
   );
 }
